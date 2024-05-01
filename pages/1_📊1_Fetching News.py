fb716ff6b92199b84f80f12c2153f549ee1afd70
aiXpertLab <aiXpertLab@gmail.com>
2024-04-30 20:37:14 -0400

    init

---
+++ pages/1_📊1_Fetching News.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from utils import st_def
+import openai, PyPDF2, os, time, pandas as pd
+
+st_def.st_logo(title='Welcome 👋 Kafka!', page_title="Kafka",)
+st.image("./images/kafka.png")
+
+
